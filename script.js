836c40548ec529734aa3559ac810a682e3b27a23
Nacho Aldama <devalladolidoficial@gmail.com>
2019-04-19 22:31:46 +0200

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-    $( "div" ).remove( "#engines" );
-$(".freegames").append("FREE");
-$(".collections").append("Collection");
-$(".prepurchase-true").append("Pre-Purchase");
+    console.log( "ready!" );
+    ( "div" ).remove( "#engines" );
+    (".freegames").append("FREE");
+    (".collections").append("Collection");
+    (".prepurchase-true").append("Pre-Purchase");
 });
