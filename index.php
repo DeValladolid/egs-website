b0b86e8be717fa903e2803f3cb4a380af9f8307e
Nacho Aldama <devalladolidoficial@gmail.com>
2019-04-20 10:41:29 +0200

    Fix Lazy animation original

--- index.php
+++ index.php
@@ -236,15 +236,51 @@ $rarityid = json_decode($file, true);
   <script type="text/javascript" src="js/mdb.min.js"></script>
   <script type="text/javascript" src="lazyload/jquery.lazy.min.js"></script>
     <script type="text/javascript" src="lazyload/jquery.lazy.plugins.min.js"></script>
-    
+  
+  
+  <script src="js/lazyload.js"></script>  
   <script type="text/javascript">
-  $(function() {
-        $('.lazy').lazy({
-          effect: "fadeIn",
-          effectTime: 500,
-          threshold: 0
-        });
-    });
+  (function() {
+				function logElementEvent(eventName, element) {
+					console.log(
+						Date.now(),
+						eventName,
+						element.getAttribute("data-src")
+					);
+				}
+
+				var callback_enter = function(element) {
+					logElementEvent("🔑 ENTERED", element);
+				};
+				var callback_exit = function(element) {
+					logElementEvent("🚪 EXITED", element);
+				};
+				var callback_reveal = function(element) {
+					logElementEvent("👁️ REVEALED", element);
+				};
+				var callback_loaded = function(element) {
+					logElementEvent("👍 LOADED", element);
+				};
+				var callback_error = function(element) {
+					logElementEvent("💀 ERROR", element);
+					element.src =
+						"https://via.placeholder.com/440x560/?text=Error+Placeholder";
+				};
+				var callback_finish = function() {
+					logElementEvent("✔️ FINISHED", document.documentElement);
+				};
+
+				var ll = new LazyLoad({
+					threshold: 0,
+					// Assign the callbacks defined above
+					callback_enter: callback_enter,
+					callback_exit: callback_exit,
+					callback_reveal: callback_reveal,
+					callback_loaded: callback_loaded,
+					callback_error: callback_error,
+					callback_finish: callback_finish
+				});
+			})();
   </script>
 
 <script src="script.js"></script>
