bea418b7ae67df263e8e236e2fc432c199960fb5
Nacho Aldama <devalladolidoficial@gmail.com>
2019-04-20 12:43:43 +0200

    vote

--- voting-system/config.php
+++ voting-system/config.php
@@ -1,10 +1,10 @@
 <?php
-$hostname = "YOUR_HOSTNAME";
-$db_username = "YOUR_USERNAME";
-$db_password = "YOUR_PASSWORD";
+$hostname = "localhost";
+$db_username = "root";
+$db_password = "cascarilla6a";
 
 $link = mysql_connect($hostname, $db_username, $db_password) or die("Cannot connect to the database");
-mysql_select_db("YOUR_DATABASE_NAME") or die("Cannot select the database");
+mysql_select_db("votes") or die("Cannot select the database");
 
 
 ?>
